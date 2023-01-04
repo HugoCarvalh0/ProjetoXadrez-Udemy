6cc6522345d3745ff851a43767129686cbfaf985
Hugo Carvalho <hugocarvmelo@gmail.com>
2023-01-04 19:38:45 -0300

    BoardException e ProgDefensiva

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,11 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		ChessMatch match = new ChessMatch();
-		
+
 		UI.printBoard(match.getPieces());
-		
+
 	}
 
 }
