1c0d5ce16cfb4aeadf6dc29132f4e259da663230
Hugo Carvalho <hugocarvmelo@gmail.com>
2023-01-07 18:53:15 -0300

    Adicionando movimentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
 
-		UI.printBoard(match.getPieces());
-
+		while (true) {
+			UI.printBoard(match.getPieces());
+			System.out.println();
+			System.out.print("Posicao de origem: ");
+			ChessPosition origem= UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Posicao de destino: ");
+			ChessPosition destino = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = match.performChessMove(origem, destino);
+		}
 	}
 }
