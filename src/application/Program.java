f76d6b98d943b5e2a21ea4f1af575868839dc54d
Hugo Carvalho <hugocarvmelo@gmail.com>
2023-03-04 11:28:19 -0300

    Correção - Tratamento para a promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,15 +17,15 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (!match.getCheckMate()) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(match,captured);
+				UI.printMatch(match, captured);
 				System.out.println();
 				System.out.print("Posicao de origem: ");
 				ChessPosition origem = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = match.possibleMoves(origem);
 				UI.clearScreen();
 				UI.printBoard(match.getPieces(), possibleMoves);
@@ -34,16 +34,20 @@ public class Program {
 				ChessPosition destino = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = match.performChessMove(origem, destino);
-				if(capturedPiece != null) {
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
-				}				
-				
-				if(match.getPromoted() != null) {
-					System.out.println("Digite a peca para promover seu peao (B/N/R/Q)");
-					String type = sc.nextLine();
+				}
+
+				if (match.getPromoted() != null) {
+					System.out.print("Digite a peca para promover seu peao (B/N/R/Q):");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Peca Invalida!! Digite a peca para promover seu peao (B/N/R/Q):");
+						type = sc.nextLine().toUpperCase();
+					}
 					match.replacePromotedPiece(type);
 				}
-				
+
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
